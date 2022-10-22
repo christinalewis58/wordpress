f4e91f9c89dd832dde8f25f0d0af0ec8be64a036
Christina Lewis <clewis13@ufl.edu>
2022-10-22 10:10:59 -0400

    initial commit

---
+++ wp-content/themes/custom-theme1/functions.php
@@ -0,0 +1,24 @@
+<?php
+/*==========================
+Add stylesheets and javascript files
+============================*/
+
+function custom_theme_scripts(){
+    //Bootstrap CSS
+    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
+    //wp_enqueue_style('bootstrap-grid', get_stylesheet_directory_uri() . '/css/bootstrap-grid.min.css');
+
+    //Main CSS
+    wp_enueue_style('main-styles', get_stylesheet_uri());
+
+    //Javascript Files
+    wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js');
+    wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/scripts.js');
+}
+
+add_action('wp_enqueue_scripts', 'custom_theme_scripts');
+
+/*==========================
+Adds Featured Images
+============================*/
+add_theme_support('post-thumbnails');
