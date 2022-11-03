07cb915755bf2edc96b948662f77a3aeaaae9dbf
Christina Lewis <clewis13@ufl.edu>
2022-11-03 19:51:05 -0400

    coded archive, header, and footer

--- wp-content/themes/custom-theme1/footer.php
+++ wp-content/themes/custom-theme1/footer.php
@@ -1,3 +1,56 @@
-    <?php wp_footer(); ?>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <?php 
+                    if(has_nav_menu('footer-left')){ ?>
+                        <nav class="footer-menu">
+                            <?php 
+                                wp_nav_menu(array(
+                                    'theme_location' => 'footer-left',
+                                ));
+                            ?>
+                        </nav>
+                    <?php }else{
+                        echo "<p>Please select a main menu through the dashboard</p>";
+                    } ?>
+
+            </div>
+
+            <div class="col-md-4">
+            <?php 
+                    if(has_nav_menu('footer-middle')){ ?>
+                        <nav class="footer-menu">
+                            <?php 
+                                wp_nav_menu(array(
+                                    'theme_location' => 'footer-left',
+                                ));
+                            ?>
+                        </nav>
+                    <?php }else{
+                        echo "<p>Please select a main menu through the dashboard</p>";
+                    } ?>
+
+            </div>
+
+            <div class="col-md-4">
+            <?php 
+                    if(has_nav_menu('footer-right')){ ?>
+                        <nav class="footer-menu">
+                            <?php 
+                                wp_nav_menu(array(
+                                    'theme_location' => 'footer-left',
+                                ));
+                            ?>
+                        </nav>
+                    <?php }else{
+                        echo "<p>Please select a main menu through the dashboard</p>";
+                    } ?>
+
+            </div>
+
+        </div>
+    </div>
+</footer>
     </body>
 </html>
--- wp-content/themes/custom-theme1/functions.php
+++ wp-content/themes/custom-theme1/functions.php
@@ -9,7 +9,7 @@ function custom_theme_scripts(){
     //wp_enqueue_style('bootstrap-grid', get_stylesheet_directory_uri() . '/css/bootstrap-grid.min.css');
 
     //Main CSS
-    wp_enueue_style('main-styles', get_stylesheet_uri());
+    wp_enqueue_style('main-styles', get_stylesheet_uri());
 
     //Javascript Files
     wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js');
@@ -31,5 +31,38 @@ $custom_header_image = array (
     'height'  => 150,
     'uploads' => true,
 );
-add_theme_support('custom-header', $custom_image_header)
-?>
+add_theme_support('custom-header', $custom_header_image);
+
+
+/*==========================
+Features Images
+============================*/
+ add_theme_support('post-thumbnails');
+
+ /*==========================
+Post Data Information
+============================*/
+function post_data(){
+    $archive_year = get_the_time('Y');
+    $archive_month = get_the_time('m');
+    $archive_day = get_the_time('d');
+
+    ?>
+    <p>Written by: <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php echo get_the_author(); ?> </a> | Published on: <a href="<?php echo get_browser_gey_link($archive_year,$archive_month,$archive_day);?> "> <?php echo "$archive_month/$archive_day/$archive_year"; ?></a></p>
+    <?php
+}
+ /*==========================
+Add menus to our theme
+============================*/
+
+function register_my_menus(){
+    register_nav_menus(array(
+        'main-menu'     => __('Main Menu'),
+        'footer-left'   => __('Left Footer Menu'),
+        'footer-middle' => __('Middle Footer Menu'),
+        'footer-right'  => __('Right Footer Menu')
+    ));
+}
+
+add_action('init', 'register_my_menus');
+ ?>
