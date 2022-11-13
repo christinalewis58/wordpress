67d07b4466cd0d39050bf8c1c9e8149fc6bbb0a2
Christina Lewis <clewis13@ufl.edu>
2022-11-13 09:10:01 -0500

    duplicated custom theme files for new theme

---
+++ wp-content/themes/assignment4-lewis-christina/functions.php
@@ -0,0 +1,116 @@
+<?php
+/*==========================
+Add stylesheets and javascript files
+============================*/
+
+function custom_theme_scripts(){
+    //Bootstrap CSS
+    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
+    //wp_enqueue_style('bootstrap-grid', get_stylesheet_directory_uri() . '/css/bootstrap-grid.min.css');
+
+    //Main CSS
+    wp_enqueue_style('main-styles', get_stylesheet_uri());
+
+    //Javascript Files
+    wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js');
+    wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/scripts.js');
+}
+
+add_action('wp_enqueue_scripts', 'custom_theme_scripts');
+
+/*==========================
+Adds Featured Images
+============================*/
+add_theme_support('post-thumbnails');
+
+/*==========================
+Custom Header Image
+============================*/
+$custom_header_image = array (
+    'width'   => 520,
+    'height'  => 150,
+    'uploads' => true,
+);
+add_theme_support('custom-header', $custom_header_image);
+
+
+/*==========================
+Features Images
+============================*/
+ add_theme_support('post-thumbnails');
+
+ /*==========================
+Post Data Information
+============================*/
+function post_data(){
+    $archive_year = get_the_time('Y');
+    $archive_month = get_the_time('m');
+    $archive_day = get_the_time('d');
+
+    ?>
+    <p>Written by: <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php echo get_the_author(); ?> </a> | Published on: <a href="<?php echo get_browser_gey_link($archive_year,$archive_month,$archive_day);?> "> <?php echo "$archive_month/$archive_day/$archive_year"; ?></a></p>
+    <?php
+}
+ /*==========================
+Add menus to our theme
+============================*/
+
+function register_my_menus(){
+    register_nav_menus(array(
+        'main-menu'     => __('Main Menu'),
+        'footer-left'   => __('Left Footer Menu'),
+        'footer-middle' => __('Middle Footer Menu'),
+        'footer-right'  => __('Right Footer Menu')
+    ));
+}
+
+add_action('init', 'register_my_menus');
+
+ /*==========================
+Pagination links
+============================*/
+function kodasContentPagination(){
+    global $wp_query;
+
+    $big = 999999999; 
+    $translated = __('page', 'mytextdomain' );
+
+    echo paginate_links( array(
+        'base' => str_replace('$big', '%#%', esc_url( get_pagenum_link( $big ))),
+        'format' => '?paged=%#%',
+        'current' => max( 1, get_query_var('paged') ),
+        'total' => $wp_query->max_num_pages,
+        'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>'
+    ) );
+}
+
+ /*==========================
+Creating Widget Areas
+============================*/
+
+function blank_widgets_init(){
+    register_sidebar(array(
+        'name'           => ('Sidebar Widget'),
+        'id'             => 'sidebar-widget',
+        'description'    => 'Area in the sidebar for content',
+        'before_widget'  => '<div class="sidebar-widget-container">',
+        'after_widget'   => '</div>',
+        'before_title'   => '<h2>',
+        'after_title'    => '</h2>'
+    ));
+
+    register_sidebar(array(
+        'name'           => ('Right Footer Widget'),
+        'id'             => 'right-footer-widget',
+        'description'    => 'Area in the right footer for content',
+        'before_widget'  => '<div class="right-footer-widget-container">',
+        'after_widget'   => '</div>',
+        'before_title'   => '<h2>',
+        'after_title'    => '</h2>'
+    ));
+
+}
+
+    add_action('widgets_init', 'blank_widgets_init');
+
+ ?>
---
+++ wp-content/themes/assignment4-lewis-christina/single.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+<main class="container">
+
+    <?php 
+    if(have_posts()){
+        while(have_posts()){
+            the_post(); ?>
+
+            <div class="single-post">
+                <div class="featured-image">
+                <?php the_post_thumbnail('large'); 
+                
+                        //Display Author and Publish Date Links
+                        post_data();
+                ?>
+                <p> <?php echo 'Post written by: ' . get_the_author() . ' | Published on: ' . get_the_date(); ?> </p>
+                </div>
+                <div class="text-container">
+                    <h2> <?php the_title(); ?></a></h2>
+
+                    <p class="body-content"> <?php echo the_content();?></p>
+                </div>
+            </div>
+
+            <?php } //end whiles
+        }
+    ?>
+</main>
+<?php get_footer(); ?>
