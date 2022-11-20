6a4e02b3267faaf54b18c8b3e02cafa3fa23edf9
Christina Lewis <clewis13@ufl.edu>
2022-11-20 14:38:24 -0500

    editing styling

--- wp-content/themes/custom-theme1/header.php
+++ wp-content/themes/custom-theme1/header.php
@@ -4,6 +4,8 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://use.typekit.net/gng0jad.css">
+        <link rel="stylesheet" href="https://use.typekit.net/gng0jad.css">
 
         <title><?php bloginfo('name'); ?></title>
         <?php wp_head(); ?>
